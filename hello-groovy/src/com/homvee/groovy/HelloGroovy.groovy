a3b186037b3c296ef63153ea5bf6a4bfa573947a
tanghomvee <tanghomvee@qq.com>
2017-04-05 18:14:23 +0800

    Add files via upload

---
+++ hello-groovy/src/com/homvee/groovy/HelloGroovy.groovy
@@ -0,0 +1,18 @@
+package com.homvee.groovy
+
+/**
+ * @Package: com.homvee.groovy
+ * @Description: TODO
+ * @date:2016 / 3 / 1 0
+ * @author: Hongwei.tang
+ * @email: Hongwei.Tang@DMall.com
+ * @Copyright: Copyright ( c ) 2016, DMall All Rights Reserved.
+ */
+public class HelloGroovy {
+
+    public static void main(String[] args) {
+        #!groovy
+       println("Hello Groovy");
+    }
+
+}
